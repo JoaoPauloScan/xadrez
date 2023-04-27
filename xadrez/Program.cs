e3c30c5e431e2f52f084817b6eba05a227c229c4
Joao Paulo Veloso <joao.souza@scanbrazilconsulting.com>
2023-04-27 14:12:36 -0300

    Print board method augumented

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -4,17 +4,13 @@ using camadaXadrez;
 
 try
 {
-    /*Tabuleiro tab = new Tabuleiro(8, 8);
+    Tabuleiro tab = new Tabuleiro(8, 8);
 
     tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
     tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(1, 3));
     tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 2));
 
-    Tela.imprimirTabuleiro(tab);*/
-
-    PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-    Console.WriteLine(pos);
-    Console.WriteLine(pos.toPosicao());
+    Tela.imprimirTabuleiro(tab);
 }
 catch(TabuleiroException e)
 {
