d8afc7b2b7167db8d754f9eade1eae072cf6d1a7
Joao Paulo Veloso <joao.souza@scanbrazilconsulting.com>
2023-04-27 11:10:38 -0300

    adding Peca to the board

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -1,6 +1,10 @@
 ﻿using tabuleiro;
 using xadrez;
+using camadaXadrez;
 
 Tabuleiro tab = new Tabuleiro(8, 8);
+
+tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(1, 3));
 
 Tela.imprimirTabuleiro(tab);
