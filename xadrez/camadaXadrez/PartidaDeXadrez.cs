6f75bfd8f7dfbe2e8ff9c4223e0a767ee5f49ec9
Joao Paulo Veloso <joao.souza@scanbrazilconsulting.com>
2023-05-02 09:13:28 -0300

    fixing Peca method

--- xadrez/camadaXadrez/PartidaDeXadrez.cs
+++ xadrez/camadaXadrez/PartidaDeXadrez.cs
@@ -115,7 +115,7 @@ namespace camadaXadrez
 
         public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-            if (!tab.peca(origem).podeMoverPara(destino))
+            if (!tab.peca(origem).movimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posição de destino inválida");
             }
